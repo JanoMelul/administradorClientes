3702eee5f9b8bfe2fd314456e51bc1784dc61146
JanoMelul <jannomelul@gmail.com>
2022-07-22 13:12:56 -0300

    first commit

---
+++ src/components/Alerta.jsx
@@ -0,0 +1,5 @@
+const Alerta = ({children}) => {
+  return <div className="text-center my-4 text-red-600">{children}</div>;
+};
+
+export default Alerta;
---
+++ src/pages/EditarCliente.jsx
@@ -0,0 +1,47 @@
+import { useState, useEffect } from "react";
+import Formulario from "../components/Formulario";
+import { useParams } from "react-router-dom";
+
+const EditarCliente = () => {
+  const [cliente, setCliente] = useState({});
+  const [cargando, setCargando] = useState(false);
+
+  const { id } = useParams();
+
+  useEffect(() => {
+    setCargando(!cargando);
+    const obtenerClientesAPI = async () => {
+      try {
+        const url = `http://localhost:4000/clientes/${id}`;
+        const respuesta = await fetch(url);
+        const resultado = await respuesta.json();
+        setCliente(resultado);
+      } catch (error) {
+        console.log(error);
+      }
+      setTimeout(() => {
+        setCargando(false);
+      }, 1000);
+    };
+
+    obtenerClientesAPI();
+  }, []);
+  return (
+    <>
+      <h1 className="font-black text-4xl text-blue-900">Editar Cliente</h1>
+      <p className="mt-3">
+        Llena los siguientes campos para editar datos de un cliente
+      </p>
+      {cliente?.nombre ? (
+        <Formulario 
+      cliente={cliente}
+      cargando={cargando}
+      />
+      ): <p>Cliente ID no válido</p>
+      }
+      
+    </>
+  );
+};
+
+export default EditarCliente;
